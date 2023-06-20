01cc41d37a9e66130882c914257be31c3c17c60f
jivaldez03 <jivaldez03@hotmail.com>
2023-06-19 21:20:19 -0600

    20230618

--- app/dt_ui_oper_gr/dt_ui_oper_bs.py
+++ app/dt_ui_oper_gr/dt_ui_oper_bs.py
@@ -220,7 +220,8 @@ def get_user_packagelist(idSCat:int, Authorization: Optional[str] = Header(None)
             ptg_errors = float(sdict["grade"]) #  - 1) * 100            
             if ptg_errors < 0:
                 ptg_errors = 100
-        if sdict["maxerrs"] > (ptg_errors if ptg_errors<0 else 100):
+        print(ptg_errors, 'maxerrs', sdict["maxerrs"])
+        if sdict["maxerrs"] > (ptg_errors if ptg_errors>=0 else 100):
             maxlevel = sdict["level"]
         else:
             maxlevel = funcs.level_seq(sdict["level"], forward=False)
